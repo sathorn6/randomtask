a4fdfeadde88f852470f19e4e1166d25e16b787f
sathorn6 <sathorn6@users.noreply.github.com>
2016-02-24 20:08:49 +0100

    Send errors to GA

--- src/analytics.js
+++ src/analytics.js
@@ -1,5 +1,20 @@
-export default {
+const analytics = {
+	_ga(...args) {
+		// Only send data in production
+		if(process && process.env && process.env.NODE_ENV == 'production')
+			ga.apply(undefined, args);
+	},
 	sendEvent(category, action) {
-		ga('send', 'event', category, action);
-	}
+		this._ga('send', 'event', category, action);
+	},
+	sendError(message, context) {
+		this.sendEvent('error', message, context);
+	},
 }
+
+// Send JS Errors to GA
+window.onerror = function(message, source, lineno, colno = 'unavailable', error = {stack:'unavailable'}) {
+	analytics.sendError(message, 'Message: "' + message + '" at ' + source + ':' + lineno + ':' + colno + '\nStack: ' + error.stack);
+}
+
+export default analytics;
